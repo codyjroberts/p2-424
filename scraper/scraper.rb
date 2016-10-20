c37168b388e324ea4c0fda8dcd087453a8cd7b88
Cody Roberts <codyjroberts@gmail.com>
2016-10-19 22:02:47 -0500

    Update data scraping method
    Add new data

--- scraper/scraper.rb
+++ scraper/scraper.rb
@@ -14,58 +14,109 @@ class RSpotify::AudioFeatures
   end
 end
 
-def find_track_id(artist, track)
-  t = RSpotify::Base.search("#{artist} #{track}", "track").find do |t|
-    String::Similarity.cosine(track, t.name) >= 0.75
-  end
+@yearly_genres = Array.new
+@yearly_tempo = Array.new
+@yearly_valence = Array.new
 
-  unless t.nil?
-    a = t.artists.find do |a|
-      String::Similarity.cosine(artist, a.name) >= 0.75
+def find_track_id(artist, track)
+  a = nil
+
+  begin
+    t = RSpotify::Base.search("#{artist} #{track}", "track").find do |t|
+      String::Similarity.cosine(track, t.name) >= 0.65
     end
+
+    unless t.nil?
+      a = t.artists.find do |a|
+        String::Similarity.cosine(artist, a.name) >= 0.65
+      end
+    end
+  rescue
+    return {
+      artist: artist,
+      track: track
+    }
   end
 
   unless t.nil? || a.nil?
-    features = t.audio_features.attributes
+    f = t.audio_features.attributes
+    genres = a.genres
+    @yearly_genres += genres
+    @yearly_tempo.push(f[:tempo])
+    @yearly_valence.push(f[:valence])
+
     {
       artist: artist,
       track: track,
-      features: features
+      spotify_artist: a.name,
+      spotify_track: t.name,
+      time_sig: f[:time_signature],
+      tempo: f[:tempo],
+      valence: f[:valence],
+      mode: f[:mode],
+      key: f[:key],
+      energy: f[:energy],
+      duration: f[:duration_ms],
+      genres: a.genres,
+      popularity: t.popularity,
+      preview: t.preview_url
     }
   else
     {
       artist: artist,
-      track: track,
-      features: nil
+      track: track
     }
   end
 end
 
-for year in 1999..2015 do
+for year in 2015..2015 do
   data = Array.new
-  page = RestClient.get("http://billboardtop100of.com/#{year}-2/")
+  begin
+    bill = RestClient.get("http://billboardtop100of.com/#{year}-2/")
+  rescue
+    next
+  end
 
-  doc = Nokogiri::HTML(page.body)
+  begin
+    wiki = RestClient.get("https://en.wikipedia.org/wiki/#{year}_in_music")
+  rescue
+    wiki = nil
+  end
+
+  bill_doc = Nokogiri::HTML(bill.body)
+  wiki_doc = Nokogiri::HTML(wiki.body)
 
   tracks = Array.new
   artists = Array.new
 
-  doc.css('table tbody td[2]').each do |a|
-    artists.push(a.text)
+  bill_doc.css('table tbody td[2]').each do |a|
+    artists.push(a.text.strip)
   end
 
-  doc.css('table tbody td[3]').each do |a|
-    tracks.push(a.text)
+  bill_doc.css('table tbody td[3]').each do |a|
+    tracks.push(a.text.strip.sub(/\nLYRICS/, ''))
   end
+
+  events = wiki_doc.at_xpath("//div[@id='mw-content-text']/ul[1]").to_html
 
   yearly_data = artists.zip(tracks).map do |i|
-    result = find_track_id(i[0], i[1])
-    next if result == nil
-    result
+    find_track_id(i[0], i[1])
   end
 
   File.open("#{year}.json","w") do |f|
-    f.puts JSON.pretty_generate({"#{year}" => yearly_data})
+    f.puts JSON.pretty_generate({
+      "#{year}" => {
+        genres: Hash.new(0).tap { |h| @yearly_genres.each { |word| h[word] += 1 } },
+        events: events,
+        avg_tempo: @yearly_tempo.grep(Float).reduce(:+) / @yearly_tempo.size.to_f,
+        avg_valence: @yearly_valence.grep(Float).reduce(:+) / @yearly_valence.size.to_f,
+        songs: yearly_data
+      }
+    })
   end
+
   puts "#{year} finished"
+  @yearly_genres = Array.new
+  @yearly_tempo = Array.new
+  @yearly_valence = Array.new
 end
